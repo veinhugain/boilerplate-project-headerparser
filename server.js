f9870590b087013c729fb7e249db16b1d7557b06
John Rutkauskas <>
2021-05-18 23:08:39 +0000

    added api/whoami  with default obj response. basically testing this push to git hub from replit

--- server.js
+++ server.js
@@ -25,6 +25,9 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/whoami", function (req, res) {
+  res.json({greeting: 'hello journeyer'});
+});
 
 
 // listen for requests :)
